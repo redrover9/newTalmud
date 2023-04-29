65f550bb1c72145aa620a2af4c68cafb66bd8c81
Grace <grace.thompson97@gmail.com>
2023-04-28 20:57:21 -0700

    fix vars

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 sefariaDB = mongoClient["sefaria"]
 sefariaDBTexts = sefariaDB["texts"]
 
-myquery = { {"versionTitle": "William Davidson Edition - English", "$nor": [{"title": "/Mishnah/i"}, {"title": "/Jerusalem Talmud Shekalim/i"}, {"title": "/Introductions to the Babylonian Talmud/i"}]} }
+masekhtotQuery = { {"versionTitle": "William Davidson Edition - English", "$nor": [{"title": "/Mishnah/i"}, {"title": "/Jerusalem Talmud Shekalim/i"}, {"title": "/Introductions to the Babylonian Talmud/i"}]} }
 
-mydoc = mycol.find(myquery)
+masekhtot = sefariaDBTexts.find(masekhtotQuery)
 
-for x in mydoc:
-  print(x)
+for pasuk in masekhtot:
+  print(pasuk)
