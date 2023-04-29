03a174cbe2024b9d3ef296ac9e2b06b94acc1fdc
Grace <grace.thompson97@gmail.com>
2023-04-28 21:57:49 -0700

    write to a file

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ sefariaDBTexts = sefariaDB["texts"]
 masekhotQuery = {"versionTitle": 'William Davidson Edition - English', "$nor": tuple([{ "title": {"$regex": "^Mishnah"}}, {"title": {"$regex": "^Jerusalem Talmud Shekalim"}}, {"title": {"$regex": "^Introductions to the Babylonian Talmud"}}])}
 masekhot = sefariaDBTexts.find(masekhotQuery)
 
-for pasuk in masekhot:
-  print(pasuk["title"])
+f = open("gemara.txt", "a")
+
+for masekhet in masekhot:
+    for chapter in masekhet["chapter"]:
+        f.write(chapter)
+f.close()
