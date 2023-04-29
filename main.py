a0d6c39a90438f22bbf0e6aa477789a708653ca8
Grace <grace.thompson97@gmail.com>
2023-04-28 21:07:05 -0700

    try to make hashable

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
 sefariaDB = mongoClient["sefaria"]
 sefariaDBTexts = sefariaDB["texts"]
 
-masekhtotQuery = { {"versionTitle": "William Davidson Edition - English", "$nor": [{"title": "/Mishnah/i"}, {"title": "/Jerusalem Talmud Shekalim/i"}, {"title": "/Introductions to the Babylonian Talmud/i"}]} }
-
+masekhtotQuery = {{"versionTitle": "William Davidson Edition - English", "$nor": tuple([{"title": "/Mishnah/i"}, {"title": "/Jerusalem Talmud Shekalim/i"}, {"title": "/Introductions to the Babylonian Talmud/i"}])}}
 masekhtot = sefariaDBTexts.find(masekhtotQuery)
 
 for pasuk in masekhtot:
